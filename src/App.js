4d8ebe2552cd68ca9abc82473a4b1d65778a43f1
tpemba100 <tpemba.96@gmail.com>
2022-12-17 16:59:45 -0500

    create routes and navigation and css(issues)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+// https://www.youtube.com/watch?v=9DDX3US3kss&t=1768s
+
+// The css import 'antd/dist/antd.css'; --> causing error
+
+
+
+
 import { Layout, Space, Typography } from "antd";
 import React from "react";
-import { Switch, Route, Link } from 'react-router-dom';
+import { Route, Link, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import Exchanges from "./components/Exchanges";
+import News from "./components/News";
+import CryptoDetails from "./components/CryptoDetails";
 
 import './App.css';
+
 
 
 
@@ -14,7 +27,20 @@ function App() {
         <Navbar />
       </div>
       <div className="main"> 
+        <Layout>
+          <div className="routes">
+            <Routes>
 
+              <Route path="home" element={<Homepage />}/>
+              <Route path="exchanges" element={<Exchanges />}/>
+              <Route path="cryptocurrencies" element={<Cryptocurrencies />}/>
+              <Route path="cryptoDetails" element={<CryptoDetails />}/>
+              <Route path="news" element={<News />}/>
+
+          
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className="footer"> 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../img/cryptocurrency.png'; 
+import MenuItem from 'antd/es/menu/MenuItem';
 
 const Navbar = () => {
   return (
@@ -12,6 +13,24 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
                 <Link to="/">Cryoto</Link>
             </Typography.Title>
+
+            <Menu theme="dark">
+              <MenuItem icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+              </MenuItem>
+              <MenuItem icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+              </MenuItem>
+              <MenuItem icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+              </MenuItem>
+              <MenuItem icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+              </MenuItem>
+            </Menu>
+          
+            
+            
             {/* <Button className="menu-control-container">
 
             </Button> */}
